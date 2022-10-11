9586d98141fb53aea05199797270df32dce00d6c
guneysrhat <guneysrhat@gmail.com>
2022-10-11 12:49:26 +0300

    search

---
+++ src/component/search/search.jsx
@@ -0,0 +1,11 @@
+import "./search.css";
+
+const Search = () => {
+  return (
+    <div className="formSearch">
+      <form action="">
+        <input type="text" placeholder="Search a city" />
+      </form>
+    </div>
+  );
+};
