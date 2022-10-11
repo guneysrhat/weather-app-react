c33eee640495e66abe8e4977ecba5510695d2345
guneysrhat <guneysrhat@gmail.com>
2022-10-11 13:49:20 +0300

    api connect

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import Search from "./component/search/Search";
 
 function App() {
   const handleOnSearchChange = (searchData) => {
-    console.log(searchData);
+    fetch(
+      `${process.env.REACT_APP_WEATHER_API_URL}/weather?q=${searchData}&appid=${process.env.REACT_APP_WEATHER_API_KEY}&units=metric`
+    )
+      .then(async (response) => {
+        const weatherResponse = await response.json();
+        console.log(weatherResponse);
+      })
+      .catch((err) => console.log(err));
   };
   return (
     <div className="container">
