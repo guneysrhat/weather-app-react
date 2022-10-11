556fabc8ea7a0dc7cd9dbe2cf1de6e7efd16849b
guneysrhat <guneysrhat@gmail.com>
2022-10-11 14:22:01 +0300

    image

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
+import { useState } from "react";
 import "./App.css";
+import Current from "./component/current/Current";
 import Search from "./component/search/Search";
 
 function App() {
-  const handleOnSearchChange = (searchData) => {
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const handleOnsearchChange = (searchData) => {
     fetch(
       `${process.env.REACT_APP_WEATHER_API_URL}/weather?q=${searchData}&appid=${process.env.REACT_APP_WEATHER_API_KEY}&units=metric`
     )
       .then(async (response) => {
         const weatherResponse = await response.json();
-        console.log(weatherResponse);
+
+        setCurrentWeather(weatherResponse);
       })
       .catch((err) => console.log(err));
   };
+
+  console.log(currentWeather);
   return (
     <div className="container">
-      <Search onSearchChange={handleOnSearchChange} />
+      <Search onSearchChange={handleOnsearchChange} />
+      <Current data={currentWeather} />
     </div>
   );
 }
